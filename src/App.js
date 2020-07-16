3b436bc2de73edb0a4ee4aa871f8465b62b4895f
Joseph Chambers <joseph@michael-chambers.com>
2020-07-15 20:11:32 -0700

    Fetch data from unsplash, save in state

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
 import 'core-js/stable';
 import 'regenerator-runtime/runtime';
 
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { hot } from 'react-hot-loader/root';
 import './styles.css';
 
-export const App = hot(() => (
-  <div className="app">
-    <h1>Unsplash Image Gallery!</h1>
+const accessKey = process.env.UNSPLASH_ACCESS_KEY;
 
-    <form>
-      <input type="text" placeholder="Search Unsplash..." />
-      <button type="button">Search</button>
-    </form>
+export const App = hot(() => {
+  const [images, setImages] = useState([]);
 
-    <div className="image-grid">
-      {[...Array(100)].map((_, index) => (
-        <div className="image" key={index}>
-          <img src="https://placekitten.com/g/1920/1080" alt="Sample" />
-        </div>
-      ))}
+  useEffect(() => {
+    fetch(`https://api.unsplash.com/photos?client_id=${accessKey}`)
+      .then(res => res.json())
+      .then(setImages);
+  }, []);
+
+  // return error if no accessKey
+  if (!accessKey) {
+    return (
+      <a href="https://unsplash.com/documentation" className="error">
+        Required: Get your Unsplash API key first.
+      </a>
+    );
+  }
+
+  return (
+    <div className="app">
+      <h1>Unsplash Image Gallery!</h1>
+
+      <form>
+        <input type="text" placeholder="Search Unsplash..." />
+        <button type="button">Search</button>
+      </form>
+
+      <div className="image-grid">
+        {images.map((image, index) => (
+          <div className="image" key={index}>
+            <img src={image.urls.regular} alt={image.alt_description} />
+          </div>
+        ))}
+      </div>
     </div>
-  </div>
-));
+  );
+});
