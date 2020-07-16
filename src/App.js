2568f4f1aa4d78d6a3f3b3bd13846c5c9b7870c2
Joseph Chambers <joseph@michael-chambers.com>
2020-07-15 19:34:00 -0700

    Setup layout.

--- src/App.js
+++ src/App.js
@@ -3,11 +3,23 @@ import 'regenerator-runtime/runtime';
 
 import React from 'react';
 import { hot } from 'react-hot-loader/root';
-import Demo from './demo';
 import './styles.css';
 
 export const App = hot(() => (
   <div className="app">
-    <Demo />
+    <h1>Unsplash Image Gallery!</h1>
+
+    <form>
+      <input type="text" placeholder="Search Unsplash..." />
+      <button type="button">Search</button>
+    </form>
+
+    <div className="image-grid">
+      {[...Array(100)].map((_, index) => (
+        <div className="image" key={index}>
+          <img src="https://placekitten.com/g/1920/1080" alt="Sample" />
+        </div>
+      ))}
+    </div>
   </div>
 ));
--- src/demo/Demo.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import './styles.scss';
-
-const Demo = () => <h1>The Code Starts Here!</h1>;
-
-export default Demo;
