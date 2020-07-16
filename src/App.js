aa73cc8582a0d2635cf26e518ceb693bcb04f03d
Joseph Chambers <joseph@michael-chambers.com>
2020-07-15 20:47:27 -0700

    Implement infinite scroll

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import 'core-js/stable';
 import 'regenerator-runtime/runtime';
 
 import React, { useEffect, useState } from 'react';
+import InfiniteScroll from 'react-infinite-scroll-component';
 import { hot } from 'react-hot-loader/root';
 import './styles.css';
 
@@ -9,12 +10,20 @@ const accessKey = process.env.UNSPLASH_ACCESS_KEY;
 
 export const App = hot(() => {
   const [images, setImages] = useState([]);
+  const [page, setPage] = useState(1);
+
+  function getPhotos() {
+    fetch(`https://api.unsplash.com/photos?client_id=${accessKey}&page=${page}`)
+      .then(res => res.json())
+      .then(data => {
+        setImages(currentImages => [...currentImages, ...data]);
+      });
+  }
 
   useEffect(() => {
-    fetch(`https://api.unsplash.com/photos?client_id=${accessKey}`)
-      .then(res => res.json())
-      .then(setImages);
-  }, []);
+    getPhotos();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [page]);
 
   // return error if no accessKey
   if (!accessKey) {
@@ -34,13 +43,26 @@ export const App = hot(() => {
         <button type="button">Search</button>
       </form>
 
-      <div className="image-grid">
-        {images.map((image, index) => (
-          <div className="image" key={index}>
-            <img src={image.urls.regular} alt={image.alt_description} />
-          </div>
-        ))}
-      </div>
+      <InfiniteScroll
+        dataLength={images.length} // This is important field to render the next data
+        next={() => setPage(page => page + 1)}
+        hasMore
+        loader={<h4>Loading...</h4>}
+      >
+        <div className="image-grid">
+          {images.map((image, index) => (
+            <a
+              className="image"
+              key={index}
+              href={image.links.html}
+              target="_blank"
+              rel="noreferrer noopener"
+            >
+              <img src={image.urls.regular} alt={image.alt_description} />
+            </a>
+          ))}
+        </div>
+      </InfiniteScroll>
     </div>
   );
 });
